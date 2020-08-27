8ae35f7972097006b515b5e8b215dedba40e2693
Vincent Sanders <vincesanders@hotmail.com>
2020-08-27 16:21:26 -0500

    Added Chuck pic.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <img src='https://nakedsecurity.sophos.com/wp-content/uploads/sites/2/2012/01/chuck-norris-thumb.jpg' alt='Chuck Norris' />
       Chuck Norris is a BAMF
     </div>
   );
